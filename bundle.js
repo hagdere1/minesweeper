50271d0c2d658e3fcffa072c04928eb748257c14
Haik Gregory Agdere <hagdere1@binghamton.edu>
2016-04-06 15:04:53 -0400

    Prevent right click menu.

--- bundle.js
+++ bundle.js
@@ -66,36 +66,33 @@
 	  this.$lElement = $lElement;
 	  this.gridLength = this.game.board.GRIDLENGTH;
 
-	  $lElement.on('click', function (event) {
+	  $lElement.on('mousedown', function (event) {
 	    $l(event.target).removeClass('hidden');
 	    $l(event.target).addClass('revealed');
 	    var pos = $l(event.target).attr('pos').split(',');
 	    var x = parseInt(pos[0]);
 	    var y = parseInt(pos[1]);
 	    var clickedTile = this.game.board.grid[x][y];
+
+
+	    if (event.button==0) {
+
+	          alert('is Left Click');
+
+	      }else if(event.button==2){
+
+	          event.preventDefault();
+	          alert('is Right Click');
+
+	      }
+
 	    clickedTile.reveal();
 
 	    if (!clickedTile.bomb && clickedTile.neighborBombCount() > 0) {
 	      $l(event.target).html(clickedTile.neighborBombCount());
 	    }
 
-	    $lDiv = $l('div');
-	    for (var row = 0; row < this.gridLength; row++) {
-	      for (var col = 0; col < this.gridLength; col++) {
-	        var tile = this.game.board.grid[row][col];
-	        if (tile.revealed) {
-	          var position = (row * this.gridLength) + col;
-	          var $lRevealedTile = $l($lDiv.elements[position]);
-	          $lRevealedTile.removeClass('hidden');
-	          $lRevealedTile.addClass('revealed');
-
-	          if (!tile.bomb && tile.neighborBombCount() > 0) {
-	            $lRevealedTile.html(tile.neighborBombCount());
-	          }
-	        }
-	      }
-	    }
-
+	    this.clearAdjacentTiles();
 	    this.game.board.isWon();
 
 	  }.bind(this));
@@ -115,6 +112,25 @@
 	      }
 
 	      this.$lElement.append($lDiv);
+	    }
+	  }
+	};
+
+	View.prototype.clearAdjacentTiles = function () {
+	  $lDiv = $l('div');
+	  for (var row = 0; row < this.gridLength; row++) {
+	    for (var col = 0; col < this.gridLength; col++) {
+	      var tile = this.game.board.grid[row][col];
+	      if (tile.revealed) {
+	        var position = (row * this.gridLength) + col;
+	        var $lRevealedTile = $l($lDiv.elements[position]);
+	        $lRevealedTile.removeClass('hidden');
+	        $lRevealedTile.addClass('revealed');
+
+	        if (!tile.bomb && tile.neighborBombCount() > 0) {
+	          $lRevealedTile.html(tile.neighborBombCount());
+	        }
+	      }
 	    }
 	  }
 	};
--- js/view.js
+++ js/view.js
@@ -3,36 +3,33 @@ var View = function (game, $lElement) {
   this.$lElement = $lElement;
   this.gridLength = this.game.board.GRIDLENGTH;
 
-  $lElement.on('click', function (event) {
+  $lElement.on('mousedown', function (event) {
     $l(event.target).removeClass('hidden');
     $l(event.target).addClass('revealed');
     var pos = $l(event.target).attr('pos').split(',');
     var x = parseInt(pos[0]);
     var y = parseInt(pos[1]);
     var clickedTile = this.game.board.grid[x][y];
+
+
+    if (event.button==0) {
+
+          alert('is Left Click');
+
+      }else if(event.button==2){
+
+          event.preventDefault();
+          alert('is Right Click');
+
+      }
+
     clickedTile.reveal();
 
     if (!clickedTile.bomb && clickedTile.neighborBombCount() > 0) {
       $l(event.target).html(clickedTile.neighborBombCount());
     }
 
-    $lDiv = $l('div');
-    for (var row = 0; row < this.gridLength; row++) {
-      for (var col = 0; col < this.gridLength; col++) {
-        var tile = this.game.board.grid[row][col];
-        if (tile.revealed) {
-          var position = (row * this.gridLength) + col;
-          var $lRevealedTile = $l($lDiv.elements[position]);
-          $lRevealedTile.removeClass('hidden');
-          $lRevealedTile.addClass('revealed');
-
-          if (!tile.bomb && tile.neighborBombCount() > 0) {
-            $lRevealedTile.html(tile.neighborBombCount());
-          }
-        }
-      }
-    }
-
+    this.clearAdjacentTiles();
     this.game.board.isWon();
 
   }.bind(this));
@@ -52,6 +49,25 @@ View.prototype.drawGrid = function () {
       }
 
       this.$lElement.append($lDiv);
+    }
+  }
+};
+
+View.prototype.clearAdjacentTiles = function () {
+  $lDiv = $l('div');
+  for (var row = 0; row < this.gridLength; row++) {
+    for (var col = 0; col < this.gridLength; col++) {
+      var tile = this.game.board.grid[row][col];
+      if (tile.revealed) {
+        var position = (row * this.gridLength) + col;
+        var $lRevealedTile = $l($lDiv.elements[position]);
+        $lRevealedTile.removeClass('hidden');
+        $lRevealedTile.addClass('revealed');
+
+        if (!tile.bomb && tile.neighborBombCount() > 0) {
+          $lRevealedTile.html(tile.neighborBombCount());
+        }
+      }
     }
   }
 };
